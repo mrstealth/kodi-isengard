c71e0af0fda76ea753a1d7e58850cf1b93d81b58
mrstealth <mrstealth[at]gmx.net>
2015-09-18 21:00:57 +0200

    cleanup

--- release.py
+++ release.py
@@ -5,14 +5,15 @@ for directory in os.listdir("."):
     if not directory.startswith('.'):
         if 'plugin' in directory or 'script' in directory or 'repository' in directory or 'skin' in directory:
             # Create plugin directory if not exist
-            zip_dir = os.path.join(os.getcwd(), 'zip', directory)
-            if not os.path.exists(zip_dir):
-                os.makedirs(zip_dir)
+            # zip_dir = os.path.join(os.getcwd(), 'zip', directory)
+            # if not os.path.exists(zip_dir):
+            #     os.makedirs(zip_dir)
 
             # Get plugin name and version from addon.xml
             addon = XML.parse('%s/addon.xml' % directory).getroot()
             name = '%s-%s.zip' %(addon.attrib['id'], addon.attrib['version'])
-            archive = os.path.join(os.getcwd(), 'zip', zip_dir, name)
+            addon_dir = os.path.join(os.getcwd(), addon.attrib['id'])
+            archive = os.path.join(addon_dir, name)
 
             if not os.path.isfile(archive):
                 print "Release new version of %s" % name
